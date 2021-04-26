928105c4342a2b3aab0135b13e85f717b8f88566
gavrileyes <gavriley-es@mail.ru>
2021-04-27 02:09:57 +0300

    lesson_2 update

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+// const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = {
   entry: path.join(__dirname, "./index.js"),
